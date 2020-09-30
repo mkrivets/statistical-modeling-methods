59d5cce026d5b8818092fadff56d2683db555ace
mkrivets <mkrivets1@gmail.com>
2020-10-01 00:34:13 +0300

    finished pseudorandom

--- task1.py
+++ task1.py
@@ -1,5 +1,8 @@
-M = 567
-m = 11234
+from scipy import stats
+
+alpha = 0.05
+M = 15
+m = 27
 
 
 def pseudorandom(x0, N):
@@ -8,14 +11,22 @@ def pseudorandom(x0, N):
     formula.
     :param x0: first number
     :param N: length of the list
-    :return:
+    :return: list
     '''
     result = []
     result.append(x0)
     for i in range(1, N):
-        x = (result[i - 1] * (M % pow(2, m))) / pow(2, m)
+        x = (result[i - 1] * (M % pow(2, m))) / (pow(2, m))
         result.append(x)
     return result
 
 
 print(pseudorandom(1, 100))
+
+stat, p = stats.normaltest(pseudorandom(1, 100))
+print("p = {:g}".format(p))
+
+if p < alpha:
+    print("The null hypothesis (our pseudorandom numbers come from a normal distribution) can be rejected")
+else:
+    print("The null hypothesis (our pseudorandom numbers come from a normal distribution) cannot be rejected")
